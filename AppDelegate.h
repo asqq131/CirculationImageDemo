064dcac88d6c99e40727e22b74d702d16ce1050a
asqq131 <395561587@qq.com>
2016-05-24 10:38:43 +0800

    Add files via upload

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CirculationImageDemo
+//
+//  Created by mac on 16/5/23.
+//  Copyright © 2016年 黄志武. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
